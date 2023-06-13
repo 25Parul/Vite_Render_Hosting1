c032cf1d6f768b2fc16a4cf10f640232830bfa0b
paruljain-25 <107891240+paruljain-25@users.noreply.github.com>
2023-06-12 23:16:30 -0700

    Render Hosting1 using vite

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+const app = express();
+const port = 3030;
+
+// fileURLToPath function converts file URL to file path string.
+import { fileURLToPath } from "url";
+
+// "import.meta.url" represents the URL of the current module.
+// function converts import.meta.url value into the current file's path.
+const __filename = fileURLToPath(import.meta.url);
+
+// path.dirname function extracts the directory path from the __filename
+const __dirname = path.dirname(__filename);
+
+
+app.use(express.static(path.join(__dirname, "/dist")))
+
+app.listen(port, ()=>{
+    console.log("Listening on port", port)
+})
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>The Power of Human Connection</h1>
+    <p>
+      In a world driven by technology and digital interactions, the power of human connection remains unparalleled. Whether through a warm smile, a compassionate touch, or an empathetic conversation, the profound impact of genuine connections transcends barriers, nurtures relationships, and enriches our lives with understanding, support, and a sense of belonging. Let us embrace the profound potential of connecting with one another and create a world where compassion, empathy, and love flourish.
+    </p>
+    </>
+  )
+}
+
+export default App
